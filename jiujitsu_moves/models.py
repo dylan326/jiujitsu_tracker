960226c551fb32e756dde13db6ef5d05d0af6b2c
Dylan Oldham <dylanoldham326@gmail.com>
2022-09-26 23:36:31 -0400

    mid work commit

--- jiujitsu_moves/models.py
+++ jiujitsu_moves/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class moves(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+    position = models.CharField(max_length=64)
