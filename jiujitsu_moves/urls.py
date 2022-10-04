0dd8fd87e48d7b659b159b00e2585b3129bf7c13
Dylan Oldham <dylanoldham326@gmail.com>
2022-10-03 21:21:07 -0400

    made and add moves link on index page

--- jiujitsu_moves/urls.py
+++ jiujitsu_moves/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('index', views.index),
-    path('', views.index)
+    path('', views.index),
+    path('add-move', views.addmove, name='add-move')
 ]
