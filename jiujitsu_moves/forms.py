f37ec4a4ad85343bb37e6a37ca5e2cedaeac5772
Dylan Oldham <dylanoldham326@gmail.com>
2022-10-07 01:33:22 -0400

    added css to forms and styled add move page

--- jiujitsu_moves/forms.py
+++ jiujitsu_moves/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class NameForm(forms.Form):
-    move_name = forms.CharField(label='Your name', max_length=100)
-    move_description = forms.CharField(widget=forms.Textarea)
+    move_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    move_description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
