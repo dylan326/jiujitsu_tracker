60f2b8803e78540fc2e0224371d65118ee90607b
Dylan Oldham <dylanoldham326@gmail.com>
2022-10-15 00:15:25 -0400

    added position select dropdown to the form

--- jiujitsu_moves/forms.py
+++ jiujitsu_moves/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
+POSITION_CHOICES = [
+    (1, 'Half Guard'),
+    (2, 'Full Guard'),
+]
 
+    
 class NameForm(forms.Form):
-    move_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    move_description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    move_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    move_description = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control'}))
+    move_position = forms.CharField(
+        label='Select Position', widget=forms.Select(attrs={'class': 'form-control'}, choices=POSITION_CHOICES))
+
