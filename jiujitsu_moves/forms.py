d0c66d5a4a97e4c36b3a91fe000428bc58ff2770
Dylan Oldham <dylanoldham326@gmail.com>
2022-10-20 00:26:39 -0400

    cleaned up code to comport with standard syntax rules

--- jiujitsu_moves/forms.py
+++ jiujitsu_moves/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from .models import moves
+from .models import Moves
 
     
-class NameForm(forms.ModelForm):
+class AddMoveForm(forms.ModelForm):
     POSITION_CHOICES = [
         (1, 'Half Guard'),
         (2, 'Full Guard'),
@@ -15,7 +15,7 @@ class NameForm(forms.ModelForm):
         label='Select Position', widget=forms.Select(attrs={'class': 'form-control'}, choices=POSITION_CHOICES))
         
     class Meta:
-        model = moves
+        model = Moves
         fields = ['name', 'description', 'position_id']
 
         
--- jiujitsu_moves/models.py
+++ jiujitsu_moves/models.py
@@ -3,25 +3,25 @@ from django.db import models
 # Create your models here.
 
 
-class moves(models.Model):
+class Moves(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=500)
     position_id = models.IntegerField()
 
 
-class bjjmoves(models.Model):
+class BjjMoves(models.Model):
     move_name = models.CharField(max_length=64)
     move_des = models.CharField(max_length=500)
     thoughts = models.CharField(max_length=500)
 
 
-class classtracker(models.Model):
+class ClassTracker(models.Model):
     date = models.DateField()
     hours = models.CharField(max_length=64)
     number_of_rolls = models.IntegerField()
     
 
-class positions(models.Model):
+class Positions(models.Model):
     position_name = models.CharField(max_length=64)
 
 
