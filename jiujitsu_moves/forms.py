4ebf7da4ae399dfc9aed446c59ec5f6be2994fd0
Dylan Oldham <dylanoldham326@gmail.com>
2022-12-08 01:30:10 -0500

    fixed models and redy to pull join

--- jiujitsu_moves/forms.py
+++ jiujitsu_moves/forms.py
@@ -1,18 +1,12 @@
 from django import forms
-from .models import Moves
+from .models import Moves, Positions
+from django.forms import ModelChoiceField
 
+class PositionsModelChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return "%s" % obj.position_name
     
 class AddMoveForm(forms.ModelForm):
-    POSITION_CHOICES = [
-        (1, 'Half Guard'),
-        (2, 'Full Guard'),
-        (3, 'Open/X Guard'),
-        (4, 'Butterfly Guard'),
-        (5, 'Worm Guard'),
-        (6, 'Side Control'),
-        (7, 'Mount'),
-        (8, 'Back'),
-    ]
     
     OFFENCE_DEFENSE = [
         (1, 'Offence'),
@@ -23,14 +17,13 @@ class AddMoveForm(forms.ModelForm):
         widget=forms.TextInput(attrs={'class': 'form-control'}))
     description = forms.CharField(
         widget=forms.Textarea(attrs={'class': 'form-control'}))
-    position_id = forms.CharField(
-        label='Select Position', widget=forms.Select(attrs={'class': 'form-control'}, choices=POSITION_CHOICES))
+    position = PositionsModelChoiceField(label='Position',widget=forms.Select(attrs={'class': 'form-control'}),queryset=Positions.objects.all(),initial = Positions.objects.get(id=1))
     is_offence = forms.CharField(
         label='Offence or Defense?', widget=forms.Select(attrs={'class': 'form-control'}, choices=OFFENCE_DEFENSE))
         
     class Meta:
         model = Moves
-        fields = ['name', 'description', 'position_id', 'is_offence']
+        fields = ['name', 'description', 'position', 'is_offence']
 
         
 
